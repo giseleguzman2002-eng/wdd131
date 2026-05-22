b1ae765348b69ccfb7287617f4018b5ea4d06615
giseleguzman2002-eng <gguzmanalvares@byupathay.edu>
2026-05-22 15:52:29 -0300

    jjk

---
+++ scripts/temples.js
@@ -0,0 +1,27 @@
+// Footer dates
+
+const year = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+year.textContent = new Date().getFullYear();
+
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+// Hamburger menu
+
+const menuButton = document.querySelector("#menuButton");
+const navigation = document.querySelector(".navigation");
+
+navigation.classList.add("hide");
+
+menuButton.addEventListener("click", () => {
+
+    navigation.classList.toggle("hide");
+
+    if (navigation.classList.contains("hide")) {
+        menuButton.textContent = "☰";
+    } else {
+        menuButton.textContent = "X";
+    }
+
+})
