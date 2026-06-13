533b35c969ac93829bd5b3b230167627b0c03fb0
giseleguzman2002-eng <gguzmanalvares@byupathay.edu>
2026-06-13 12:34:29 -0300

    d

--- scripts/form.js
+++ scripts/form.js
@@ -36,14 +36,7 @@ products.forEach(product => {
 
     productSelect.appendChild(option);
 });
-let reviewCount = Number(localStorage.getItem("reviewCount")) || 0;
 
-reviewCount++;
-
-localStorage.setItem("reviewCount", reviewCount);
-
-document.querySelector("#reviewCount").textContent = reviewCount;
-// Footer
 document.querySelector("#year").textContent =
 new Date().getFullYear();
 
